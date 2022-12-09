435a2652ed546a0f85050a70e4a7ddccd550e80b
Temitope Bamidele <temibami@gmail.com>
2022-12-09 12:00:03 +0100

    initiated

---
+++ products.js
@@ -0,0 +1,30 @@
+const products = [
+    {
+        name: 'Fendi Shoes',
+        price: '4000',
+        image: [
+            'https://cdn-images.farfetch-contents.com/13/67/27/32/13672732_21610772_1000.jpg',
+            'https://cdn-images.farfetch-contents.com/13/67/27/32/13672732_21610776_1000.jpg',
+            'https://cdn-images.farfetch-contents.com/13/67/27/32/13672732_21610783_1000.jpg',
+        ],
+    },
+    {
+        name: 'Gucci Bags',
+        price: '8000',
+        image: [
+            'https://cdn-images.farfetch-contents.com/14/83/62/85/14836285_24015797_1000.jpg',
+            'https://cdn-images.farfetch-contents.com/14/83/62/85/14836285_24015811_1000.jpg',
+            'https://cdn-images.farfetch-contents.com/14/83/62/85/14836285_24015816_1000.jpg',
+        ],
+    },
+];
+
+
+function name(params) {
+    params()
+}
+function noni() {
+    return 'hi';
+}
+
+name(noni);
